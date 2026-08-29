0bfa7d3603f3838e5ce28293ef5f7882673ad144
NelikKKL <smerdinmacsim@gmail.com>
2026-08-29 17:07:43 +0300

    Last Sync: 2026-08-29 17:07 (Mobile)

--- src/main.rs
+++ src/main.rs
@@ -19,6 +19,6 @@ fn main() -> eframe::Result<()> {
     eframe::run_native(
         "yt-dlp GUI",
         options,
-        Box::new(|cc| Ok(Box::new(YtDlpApp::new(cc)) as Box<dyn eframe::App>)),
+        Box::new(|cc| Box::new(YtDlpApp::new(cc)) as Box<dyn eframe::App>),
     )
 }
